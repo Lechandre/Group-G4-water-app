11aaf7ae99e99c8bcbba1689c8dbc62b1eeb6cc0
Lechandre <whitneycloete27@gmail.com>
2021-11-28 21:24:10 +0200

    tenth commit

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 
 let express = require('express');
 let app = express();
-const exphbs  = require('express-handlebars');
-//const exphbs = require('express-handlebars'); 
+const exphbs = require('express-handlebars');
+//const exphbs = require('express-handlebars');
+
 
 const sqlite3 = require('sqlite3');
-const { open } = require('sqlite'); 
+const { open } = require('sqlite');
 
 //app.get("/", function(req, res){
-  //res.send("Water Usage WebApp");
+//res.send("Water Usage WebApp");
 //});
 
 
 
 //app.engine('handlebars', exphbs({
-  //layoutsDir : './views/layouts'
+//layoutsDir : './views/layouts'
 //})); 
 
 //app.set('view engine', 'handlebars'); 
@@ -24,9 +25,9 @@ const path = require('path');
 //Configuring express handlebars
 var hbs = exphbs.create({
   helpers: {
-      getStringifiedJson: function (value) {
-          return JSON.stringify(value);
-      }
+    getStringifiedJson: function (value) {
+      return JSON.stringify(value);
+    }
   },
   defaultLayout: 'main',
   partialsDir: ['views/partials/']
@@ -45,49 +46,50 @@ app.use(bodyParser.json())
 app.use(express.static('public'));
 
 //app.get("/", function(req, res){
-  
-  //res.send("Water Usage App");
+
+//res.send("Water Usage App");
 //});
 
 
 open({
-  filename: './usage.db', 
+  filename: './usage.db',
   driver: sqlite3.Database
 }).then(async function (db) {
-  await db.migrate(); 
+  await db.migrate();
 
   app.get('/', async function (req, res) {
-    
-  
-const register = await db.all('select * from register')
-res.render('login-screen', {
-  register
 
-}); 
-  
 
-    
+    const register = await db.all('select * from register')
+    res.render('login-screen', {
+      register
+
+    });
+
+
+
     //.then(function(register){
 
-      //console.log(register); (this shows the whole database on the terminal)
+    //console.log(register); (this shows the whole database on the terminal)
 
-    
-       
+
+
     //})
 
   });
-    
-    
-    app.get('/login-screen', function (req, res) {
-      res.render('login-screen');
-    });
 
-    app.get('/register-screen', function (req, res) {
-      res.render('register-screen');
-    });
-  
+
+  app.get('/login-screen', function (req, res) {
+    res.render('login-screen');
+  });
+
+  app.get('/register-screen', function (req, res) {
+    res.render('register-screen');
+  });
+
   app.get('/leak-screen', function (req, res) {
     res.render('leak-screen');
+
   });
 
   app.get('/weekly-usage-screen', function (req, res) {
@@ -97,163 +99,176 @@ res.render('login-screen', {
   app.get('/month-usage-screen', function (req, res) {
     res.render('month-usage-screen');
   });
-  
+
+  app.get('/alert-screen', function (req, res) {
+    res.render('template', { usageList: arr })
+  });
+
   app.get('/history-screen', function (req, res) {
     res.render('history-screen');
 
     db
-    .all('select * from history')
-    .then(function(history){
+      .all('select * from history')
+      .then(function (history) {
 
-      console.log(history); 
+        console.log(history);
 
-    
-       
-    })
 
-    
+
+      })
+
+
   });
-  
-  
+
+
 
   app.get('/daily-usage-screen', function (req, res) {
     res.render('daily-usage-screen');
   });
 
+
+
+
   //app.post('/login',  function (req, res) {
-    
-    //console.log(req.body); 
-  
-    //res.redirect('daily-usage-screen'); 
+
+  //console.log(req.body); 
+
+  //res.redirect('daily-usage-screen'); 
   //});
 
-  app.post('/login',  function (req, res) {
-    
-    console.log(req.body); 
-  
-    res.redirect('leak-screen'); 
+  app.post('/login', function (req, res) {
+
+    console.log(req.body);
+
+    res.redirect('leak-screen');
   });
 
-  app.post('/register-screen',  function (req, res) {
-    
-    console.log(req.body); 
+  app.post('/alert-screen', function (req, res) {
 
+    console.log(req.body);
 
-
-  res.redirect('leak-screen');  
-  
+    res.redirect('alert-screen');
   });
 
-  app.post('/registration',  function (req, res) {
-    
-    console.log(req.body); 
+  app.post('/register-screen', function (req, res) {
+
+    console.log(req.body);
 
 
 
-  res.redirect('register-screen');  
-  
+    res.redirect('leak-screen');
+
   });
 
-  app.post('/leak-screen',  function (req, res) {
-    
-    console.log(req.body); 
+  app.post('/registration', function (req, res) {
+
+    console.log(req.body);
+
+
+
+    res.redirect('register-screen');
+
+  });
+
+  app.post('/leak-screen', function (req, res) {
+
+    console.log(req.body);
 
     res.redirect('daily-usage-screen');
-  
-  }); 
-  
-  app.post('/history-screen',  function (req, res) {
-      
-      console.log(req.body); 
 
-      res.redirect('history-screen');
-  
-    });
-  
-    
-
-  app.post('/daily-usage-screen',  function (req, res) {
-    
-    console.log(req.body); 
-
-    res.redirect('daily-usage-screen');
-  
   });
 
-  app.post('/weekly-usage-screen',  function (req, res) {
-    
-    console.log(req.body); 
+  app.post('/history-screen', function (req, res) {
+
+    console.log(req.body);
+
+    res.redirect('history-screen');
+
+  });
+
+
+
+  app.post('/daily-usage-screen', function (req, res) {
+
+    console.log(req.body);
+
+    res.redirect('daily-usage-screen');
+
+  });
+
+  app.post('/weekly-usage-screen', function (req, res) {
+
+    console.log(req.body);
 
     res.redirect('weekly-usage-screen');
-  
+
   });
 
-  app.post('/month-usage-screen',  function (req, res) {
-    
-    console.log(req.body); 
+  app.post('/month-usage-screen', function (req, res) {
+
+    console.log(req.body);
 
     res.redirect('month-usage-screen');
-  
+
   });
 
+  app.post('/submit_usage', function (req, res) {
 
-})
+    const latest_usage = req.body.latest_usage;
+    usage.push(usage);
+
+    // run the algo for checking if outlier
+
+    // use your existing algorithm code
+    const arr = [5, 7, 6, 5, 6, 6, 0];
+    const outlier = stats.indexOfOutliers(arr)  // Default theshold of 3
+    last = arr[arr.length - 1]
+    console.log(outlier)
+
+    is_outlier = arr[outlier] == last
+    if (is_outlier) {
+      if (last > stats.mean(arr)) {
+        console.log("Too much")
+      }
+      else {
+        console.log("Saved")
+      }
+    }
+    else {
+      console.log("Normal")
+    }
+    if (outlier == null) {
+      console.log(arr[outlier])
+    }
+
+
+    let messageStyle = "";
+
+    let message = "";
+    if (type === "saved") {
+      messageStyle = "saved"
+      message = "Well done, you saved water"
+    }
+
+    if (type === "wasted") {
+      messageStyle = "wasted"
+      message = "Yikes you used more water this month. Slow down!"
+    }
+
+    if (message === "") {
+      message = "Your usage is consistent with your past water usage."
+    }
+
+    res.render('usage', {
+      usage,
+      messageStyle,
+      message
+    });
+  });
+
 
 let PORT = process.env.PORT || 3007;
 
-app.listen(PORT, function(){
-  console.log('App starting on port', PORT);
-});
-
-
-
-//const stats = require("stats-analysis") // include statistics library
-
-//const arr = [5, 7, 6, 5, 6, 6, 0];
-//const outlier = stats.indexOfOutliers(arr)  // Default theshold of 3
-//last = arr[arr.length - 1]
-//console.log(outlier)
-
-//is_outlier = arr[outlier]  == last
-//if(is_outlier)
-//{
-    //if(last > stats.mean(arr))
-    //{
-        //console.log("Too much")
-    //}
-    //else
-    //{
-        //console.log("Saved")
-    //}
-//}
-//else
-//{
-    //console.log("Normal")
-//}
-//if(outlier ==null)
-//{
-    //console.log(arr[outlier])
-//}
-
-
-
-
-
-//app.get('/', function (req, res) {
-  //res.render('home');
-//});
-
-
-
-
-
-
-
-
-
-
-
-
-  
-
-
+  app.listen(PORT, function () {
+    console.log('App starting on port', PORT);
+  });
--- public/leak-screen.js
+++ public/leak-screen.js
@@ -5,20 +5,28 @@ const historyBtn = document.querySelector(".historyBtn");
 const leakBtn = document.querySelector(".leakBtn");
 const setBtn = document.querySelector(".setBtn");
 const weekBtn = document.querySelector(".weekBtn");
+const feedbackBtn = document.querySelector(".feedbackBtn");
 
 weekBtn.addEventListener('click', function() {
     //alert("hello")
 });
 
-usageBtn.addEventListener('click', function() {
-    alert("hello")
-});
+//usageBtn.addEventListener('click', function() {
+    //alert("hello")
+//});
 
 historyBtn.addEventListener('click', function() {
 alert("hello")
 });
 
 
-setBtn.addEventListener('click', function() {
-    alert("hello")
-    });
+//setBtn.addEventListener('click', function() {
+    //alert("hello")
+    //});
+
+feedbackBtn.addEventListener('click', function() {
+        alert("hello")
+        });
+
+
+        
